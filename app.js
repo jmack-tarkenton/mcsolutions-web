b191627b8e6d3c6f0f03065ad796bcf80eb5c309
jtmccormick18 <josh.mccormick81@gmail.com>
2023-04-11 14:26:21 -0400

    updates:

--- app.js
+++ app.js
@@ -2,13 +2,12 @@ const express = require("express");
 const path = require("path");
 const app = express();
 
-// const http=require("http");
 
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-app.use(express.static(path.join(__dirname, "client-static")));
+app.use(express.static(path.join(__dirname, "client-static"),{dotfiles:'allow'}));
 
 app.use("/", function (req, res) {
     res.sendFile(path.join(__dirname, "client-static", "index.html"));
@@ -16,12 +15,3 @@ app.use("/", function (req, res) {
 
 module.exports = app;
 
-// for development and debugging
-
-// const server=http.createServer(app);
-
-// server.listen();
-
-// app.listen(PORT, function () {
-//     console.log(`App listening on PORT ${PORT}`);
-// });
--- server.js
+++ server.js
@@ -1,12 +1,9 @@
-// const http = require("http");
+
 require('dotenv').config();
 const PORT = process.env.PORT || 3000;
 
 
 const app = require('./app');
 
-// const greenlock=require('./greenlock');
-
-// greenlock.serve(app);
 
 app.listen(PORT,()=>`App Listening on ${PORT}`);
