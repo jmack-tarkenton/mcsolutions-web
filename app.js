43c4f8744e5969b4d449d6cd4330bb75a59721f3
jtmccormick18 <josh.mccormick81@gmail.com>
2024-01-02 09:56:55 -0500

    initial

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ app.use(express.json());
 app.use(express.static(path.join(__dirname, "client-static"),{dotfiles:'allow'}));
 
 app.use("/", function (req, res) {
-    res.sendFile(path.join(__dirname, "client-static", "index.html"));
+    res.sendFile(path.join(__dirname, "web", "index.html"));
 })
 
 module.exports = app;
