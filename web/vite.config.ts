90c90125c7452fb271e5a85f98196cd97f3aa201
jtmccormick18 <josh.mccormick81@gmail.com>
2024-01-02 10:55:25 -0500

    changes

--- web/vite.config.ts
+++ web/vite.config.ts
@@ -1,7 +1,19 @@
 import { defineConfig } from 'vite'
+import { fileURLToPath, URL } from 'url';
 import react from '@vitejs/plugin-react'
+import path from 'path'
+
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  root: __dirname,
+  resolve: {
+    alias: {
+      '~bootstrap': path.resolve(__dirname, 'node_modules/bootstrap'),
+      '@': fileURLToPath(new URL('./src', import.meta.url)),
+
+    }
+  },
   plugins: [react()],
 })
